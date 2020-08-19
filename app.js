4c7ea23b61678c6c3bfb31a7e0689114c736a817
kiran-nepali <cuziamnepali@hotmail.com>
2020-08-19 15:08:08 +0100

    Added forms

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-var jobCharacter = [];
-var indivSkill = [];
+
 var matchScore = 0;
 
-function SkillMatch(jobCharacter =[],indivSkill=[]){
-    for(var i = 0;i<jobCharacter.length;i++){
+function SkillMatch(){
+    var jobChar = document.getElementsByName('jobChar[]');
+    var indivSkill = document.getElementsByName('indivSkill[]'); 
+    for(var i = 0;i<jobChar.length;i++){
         for(var j = 0;j<indivSkill.length;j++){
-            if (jobCharacter[i]===indivSkill[j]){
+            if (jobChar[i].value===indivSkill[j].value){
                 matchScore++;
                 // console.log("The skillset matches with the person")
             }
         }
     }
-    avgMatchScore = (matchScore/jobCharacter.length) * 100;
+    avgMatchScore = (matchScore/jobChar.length) * 100;
     if (avgMatchScore === 0){
+        document.getElementById("result").innerHTML = "The person doesn't match our skill";
         console.log("The person doesn't match our skill");
     }
     else if(avgMatchScore === 100){
+        document.getElementById("result").innerHTML = "The person matches our skill set";
         console.log("The person matches our skill set");
     }
     else{
+        document.getElementById("result").innerHTML = "The Skillset match score for this person is "+avgMatchScore;
         console.log("The Skillset match score for this person is "+avgMatchScore);
     }
 }
 
-SkillMatch(["remote","parttime","flexible"],["flexible","remote"]);
+// SkillMatch(["remote","parttime","flexible"],["flexible","remote"]);
