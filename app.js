38f9c10073b11fb30260447e21e29f69e82f7d73
kiran-nepali <cuziamnepali@hotmail.com>
2020-08-19 14:17:30 +0100

    Added an index html and app js file

---
+++ app.js
@@ -0,0 +1,26 @@
+var jobCharacter = [];
+var indivSkill = [];
+var matchScore = 0;
+
+function SkillMatch(jobCharacter =[],indivSkill=[]){
+    for(var i = 0;i<jobCharacter.length;i++){
+        for(var j = 0;j<indivSkill.length;j++){
+            if (jobCharacter[i]===indivSkill[j]){
+                matchScore++;
+                // console.log("The skillset matches with the person")
+            }
+        }
+    }
+    avgMatchScore = (matchScore/jobCharacter.length) * 100;
+    if (avgMatchScore === 0){
+        console.log("The person doesn't match our skill");
+    }
+    else if(avgMatchScore === 100){
+        console.log("The person matches our skill set");
+    }
+    else{
+        console.log("The Skillset match score for this person is "+avgMatchScore);
+    }
+}
+
+SkillMatch(["notremote","flexible"],["flexible","remote"]);
